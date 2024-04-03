448c6d8b4450d1b5d10c3ba63bd32ce7820faaa8
Kusal Thiwanka <mhkusalthiwanka@gmail.com>
2024-04-02 21:05:44 -0400

    Add clo745 project sample webserver script file

---
+++ clo745/project/webserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install httpd -y
+sudo yum install php -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo wget -P /var/www/html/ https://raw.githubusercontent.com/KusalThiwanka/CAA-Code-Snippet/master/clo745/project/index.php
+sudo systemctl restart httpd
